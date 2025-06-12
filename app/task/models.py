84084ed1b98e8e5afc1a65ea85a9381405406560
unknown <akhtar.decy@gmail.com>
2025-06-12 15:20:48 +0530

    v2 done

--- app/task/models.py
+++ app/task/models.py
@@ -1,7 +1,18 @@
 from sqlmodel import SQLModel,Field
 
+class TaskBase(SQLModel):
+    title :str
+    content :str
+    
+class TaskCreate(TaskBase):
+    pass
 
-class Task(SQLModel,table=True):
+class TaskUpdate(TaskBase):
+    pass 
+
+class TaskOut(TaskBase):
     id : int = Field(primary_key=True)
-    title : str
-    content : str 
+
+
+class Task(TaskBase,table=True):
+    id : int = Field(primary_key=True)
--- app/task/services.py
+++ app/task/services.py
@@ -1,36 +1,37 @@
 from app.db.config import engine
 from app.task.models import Task
 from sqlmodel import Session,select
+from app.task.models import *
 
-def create_task(title:str,content:str):
+def create_task(new_task:TaskCreate) -> TaskOut:
     with Session(engine)  as sesion:
-        task = Task(title=title,content=content)
+        task = Task(new_task)
         sesion.add(task)
         sesion.commit()
         sesion.refresh(task)
         return task
     
-def get_all_task():
+def get_all_task() -> TaskOut:
     with Session(engine) as session:
         stmt = select(Task)
         tasks = session.exec(stmt).all()
         return tasks
     
-def get_task_by_id(id:int):
+def get_task_by_id(id:int) -> TaskOut:
     with Session(engine) as session:
         task = session.get_one(Task,id)
         return task
     
-def update_task(id:int,title:str,content:str):
+def update_task(id:int,new_task:TaskUpdate) -> TaskOut: 
     with Session(engine) as session:
         task = session.get_one(Task,id)
-        task.title = title
-        task.content = content
+        task.title = new_task.title
+        task.content = new_task.content
         session.commit()
         session.refresh(task)
         return task
     
-def delete_task(id:int):
+def delete_task(id:int) -> bool:
     with Session(engine) as session:
         task = session.get_one(Task,id)
         session.delete(task)
